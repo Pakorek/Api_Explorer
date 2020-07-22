cd63245148e746d04f1f8e72fb0c212a90fcca91
pakorek <brice.darmenia@sfr.fr>
2020-07-22 18:34:04 +0200

    create Entities and Repositories

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,6 +1,12 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\ApiActor;
+use App\Entity\ApiCategory;
+use App\Entity\ApiCreator;
+use App\Entity\ApiEpisode;
+use App\Entity\ApiProgram;
+use App\Entity\ApiSeason;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,6 +30,18 @@ class ApiController extends AbstractController
     public function index():Response
     {
         return $this->render('admin/index.html.twig');
+    }
+
+    public function getAllApiRepo():array
+    {
+        return $repos = [
+            'api_program' => $this->getDoctrine()->getRepository(ApiProgram::class)->findAll(),
+            'api_season' => $this->getDoctrine()->getRepository(ApiSeason::class)->findAll(),
+            'api_episode' => $this->getDoctrine()->getRepository(ApiEpisode::class)->findAll(),
+            'api_actor' => $this->getDoctrine()->getRepository(ApiActor::class)->findAll(),
+            'api_creator' => $this->getDoctrine()->getRepository(ApiCreator::class)->findAll(),
+            'api_category' => $this->getDoctrine()->getRepository(ApiCategory::class)->findAll()
+        ];
     }
 
 
