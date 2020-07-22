8f56304632642b9f6ac4d96bd6fb71be85007473
pakorek <brice.darmenia@sfr.fr>
2020-07-22 17:30:09 +0200

    create user (make:user), customised and linked with billing plan (ManyToOne)

--- src/Entity/BillingPlan.php
+++ src/Entity/BillingPlan.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\BillingPlanRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,16 @@ class BillingPlan
      * @ORM\Column(type="integer")
      */
     private $price;
+
+    /**
+     * @ORM\OneToMany(targetEntity=User::class, mappedBy="billing_plan")
+     */
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +64,37 @@ class BillingPlan
     public function setPrice(int $price): self
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->setBillingPlan($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->contains($user)) {
+            $this->users->removeElement($user);
+            // set the owning side to null (unless already changed)
+            if ($user->getBillingPlan() === $this) {
+                $user->setBillingPlan(null);
+            }
+        }
 
         return $this;
     }
