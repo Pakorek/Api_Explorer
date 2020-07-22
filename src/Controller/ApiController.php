f78693cdedd7e331dba8b4a3d949071f0929dfad
pakorek <brice.darmenia@sfr.fr>
2020-07-22 18:30:01 +0200

    create tables for movie api, with api's tables to 'cache' if needed

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,7 +1,30 @@
 <?php
 namespace App\Controller;
 
-class ApiController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/API", name="api_")
+ *
+ * Class ApiController
+ * @package App\Controller
+ */
+class ApiController extends AbstractController
 {
+    const KEY = 'k_Gyn239Fh';
+
+
+    /**
+     * @Route("/", name="index")
+     *
+     * @return Response
+     */
+    public function index():Response
+    {
+        return $this->render('admin/index.html.twig');
+    }
+
 
 }
