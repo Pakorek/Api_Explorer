ae03e666e04853f9843af1de2422939e1df9310f
pakorek <brice.darmenia@sfr.fr>
2020-07-23 14:54:55 +0200

    make:registration and require verify-email-bundle

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -49,6 +51,11 @@ class User implements UserInterface
      * @ORM\JoinColumn(nullable=false)
      */
     private $billing_plan;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function getId(): ?int
     {
@@ -155,6 +162,18 @@ class User implements UserInterface
     public function setBillingPlan(?BillingPlan $billing_plan): self
     {
         $this->billing_plan = $billing_plan;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
