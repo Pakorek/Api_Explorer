a41e9c55c443f0a62ca9c1d1f2b50fec441bc491
pakorek <brice.darmenia@sfr.fr>
2020-07-25 16:36:37 +0200

    fix assert on BugReport Entity

--- src/Entity/BugReport.php
+++ src/Entity/BugReport.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\BugReportRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BugReportRepository::class)
@@ -19,6 +20,7 @@ class BugReport
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Mmmh, un message sans message ?")
      */
     private $message;
 
